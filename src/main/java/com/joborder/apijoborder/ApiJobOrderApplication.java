3f8a23293117045ebc4f049948c4f8896e39ba9d
Ana Carolina <carol.oliva.cruz@gmail.com>
2024-03-04 18:34:29 -0300

    start: project startup

---
+++ src/main/java/com/joborder/apijoborder/ApiJobOrderApplication.java
@@ -0,0 +1,13 @@
+package com.joborder.apijoborder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJobOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJobOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joborder/apijoborder/ApiJobOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joborder.apijoborder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJobOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
